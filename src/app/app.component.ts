2adac524e8258562cce7c22245d909e24333fd55
Alberto Marin <alberto.marind@gmail.com>
2021-01-27 17:19:25 -0600

    practica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CatalogoPais } from './catalogo-pais';
+import { ConsultaPais } from './consulta-pais';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'hello-world';
+export class AppComponent implements OnInit {
+  paisSeleccionado: CatalogoPais;
+  consultaPais: ConsultaPais = new ConsultaPais();
+  constructor() {
+    this.paisSeleccionado = null;
+  }
+  ngOnInit(): void {
+    this.paisSeleccionado = this.consultaPais.getPaisByName('eeuu');
+    console.log(this.paisSeleccionado);
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NuevoComponent } from './nuevo/nuevo.component';
+import { Server1Commponent } from './server1';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NuevoComponent,
+    Server1Commponent
   ],
   imports: [
     BrowserModule
